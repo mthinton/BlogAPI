78716521adbbc3043bff579dc18fb7f738b52d54
mthintoncoding <mthinton@gmail.com>
2017-04-07 13:06:23 -0400

    added POST test

--- test/test.js
+++ test/test.js
@@ -16,20 +16,30 @@ describe('blog-posts', function(){
 		return closeServer();
 	});
 
-  it('should list items on GET', function(done) {
-    chai.request(app)
+  it('should list blog posts on GET', function() {
+    return chai.request(app)
       .get('/blog-posts')
-      .end(function(err, res) {
+      .then(function(res) {
         res.should.have.status(200);
         res.should.be.json;
         res.body.should.be.a('array');
-        res.body.length.should.be.above(0);
-        res.body.forEach(function(item) {
+        res.body.forEach(function (item){
           item.should.be.a('object');
-          item.should.have.all.keys(
-            'id', 'title', 'content', 'author', 'publishDate')
+          item.should.have.all.keys('id', 'title', 'content', 'author', 'publishDate')
         });
       });
-    done();
+  });
+
+   it('should post new blog posts on POST', function(){
+    const newItem = {author: "Chris Cauley", title: "Programming in CSS", content: "It's that easy", publishDate: "11/30/2016"};
+    return chai.request(app)
+    .post('/blog-posts')
+    .send(newItem)
+    .then(function(res){
+      res.should.have.status(201);
+      res.should.be.json;
+      res.should.be.a('object');
+      res.body.should.have.all.keys('id', 'title', 'content', 'author', 'publishDate')
+    });
   });
 })
