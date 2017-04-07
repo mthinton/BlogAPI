947d30202071742068845eafe7e25f39c8dfe928
mthintoncoding <mthinton@gmail.com>
2017-04-06 21:49:35 -0400

    added test module and travis.yml file

---
+++ test/blog-tester.js
@@ -0,0 +1,28 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('blog-posts', function(){
+	before(function(){
+		return runServer();
+	});
+
+	after(function(){
+		return closeServer();
+	});
+
+	it('should list blog recipes on GET', function(){
+		return chai.request(app)
+		.get('/blog-posts')
+		.then(function(res){
+			res.should.have.status(200);
+			res.should.be.json;
+
+		})
+	})
+})
